7653f4a259f77193ec46e5ee70307d4389bd2f6d
irdepla <rixsiboyevislombek@gmail.com>
2024-10-07 04:46:41 +0500

    started

---
+++ script.js
@@ -0,0 +1,28 @@
+// Example 1
+
+// const a = +prompt("Son kiriting:")
+
+// function square(a) {
+//    return a**2;
+// }
+// console.log(square(a));
+
+
+// Example 2
+
+
+// let a = +prompt("Son kiriting:")
+// let b = +prompt("Son kiriting:")
+
+// let sum = (a,b) => a + b;
+// console.log(sum(a,b));
+
+
+// Example 3
+
+
+const name = prompt("Ismingizi kiriting:")
+function greeting(name = "Do'stim") {
+    return `Salom ${name}`;
+}
+console.log(greeting(name));
